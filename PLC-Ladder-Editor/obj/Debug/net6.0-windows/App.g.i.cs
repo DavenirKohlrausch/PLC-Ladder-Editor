25cd6ab76b63e91ea05c0eb01ad6152eb9e36f81
Davenir Kohlrausch <davenir.k@gmail.com>
2025-09-22 21:34:29 -0300

    2209

--- PLC-Ladder-Editor/obj/Debug/net6.0-windows/App.g.i.cs
+++ PLC-Ladder-Editor/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace PLC_Ladder_Editor {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.8.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace PLC_Ladder_Editor {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.8.0")]
         public static void Main() {
             PLC_Ladder_Editor.App app = new PLC_Ladder_Editor.App();
             app.InitializeComponent();
